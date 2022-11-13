aa3953400a871b8203ed52801e850839cd7571d5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-13 14:31:46 +0530

    adding new contact

--- Program.cs
+++ Program.cs
@@ -5,21 +5,72 @@ namespace AddresBookSystemm
     public class Program
     {
         //creating list of contact
-        public static List<Contact> ContactList = new List<Contact>();
+        //public static List<Contact> contactList = new List<Contact>();
+        //    public static void Main(string[] args)
+        //    {
+        //        Console.WriteLine("Welcome to Address Book Sytem.");
+
+        //        ability to creating contacts
+        //        Contact contact1 = new Contact();
+        //        contact1.firstName = "Anurag";
+        //        contact1.lastName = "Mendhe";
+        //        contact1.address = "Akola Maharashtra";
+        //        contact1.phone = 1234567890;
+        //        contact1.city = "Akola";
+        //        contact1.state = "Maharashtra";
+        //        contact1.zipcode = 444005;
+        //        contact1.email = "charu@gmail.com"
+        //        contactList.Add(contact1);
+        //    }
+        //}
+
+
+
+        public static List<Contact> contactList = new List<Contact>();
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Sytem.");
 
-            //ability to creating contacts
-            Contact contact = new Contact();
-            contact.firstName = "Anurag";
-            contact.lastName = "Mendhe";
-            contact.address = "Akola Maharashtra";
-            contact.phone = 1234567890;
-            contact.city = "Akola";
-            contact.state = "Maharashtra";
-            contact.zipcode = 444005;
-            ContactList.Add(contact);
+            Console.WriteLine("Do you want to add new contact press 1 or press 2 to cancle.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num == 1)
+            {
+                Contact contact1 = new Contact();
+                Console.WriteLine("Enter first name: ");
+                contact1.firstName = Console.ReadLine();
+                Console.WriteLine("Enter last name: ");
+                contact1.lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter address: ");
+                contact1.address = Console.ReadLine();
+
+                Console.WriteLine("Enter city: ");
+                contact1.city = Console.ReadLine();
+
+                Console.WriteLine("Enter state: ");
+                contact1.state = Console.ReadLine();
+
+                Console.WriteLine("Enter phone: ");
+                contact1.phone = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter email: ");
+                contact1.email = Console.ReadLine();
+
+                Console.WriteLine("Enter zipcode: ");
+                contact1.zipcode = Convert.ToInt32(Console.ReadLine());
+                contactList.Add(contact1);
+
+                Console.WriteLine("Do you want to add anoter contact then press 1 or press 2 for exit ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Total number of contact in address book:" + contactList.Count);
+
+            foreach (Contact contact in contactList)
+            {
+                Console.WriteLine(contact.firstName);
+            }
+
         }
     }
 }
